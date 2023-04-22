99571e5dd3333ba5e5586775c3966a882982047f
kume takafumi <46054448+kume-takafumi@users.noreply.github.com>
2023-04-22 04:05:07 +0300

    Create iic.c

---
+++ multiple_object_file_link/cython_library/iic/iic.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//----------------------------------------s.
+void hellow_iic_cpp() {
+    printf("void hellow_iic_cpp();\n");
+};//--------------------------------------e.
